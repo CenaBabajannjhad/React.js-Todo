d7f06690ebea3af034f5e4c68d685343c1d9fed2
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-08-21 17:31:06 +0330

    add ui change task close button

--- src/components/EditPanel.jsx
+++ src/components/EditPanel.jsx
@@ -4,6 +4,10 @@ import EditForm from './EditForm'
 const EditPanel = ({sendEditedTask , id}) => {
   return (
     <section className='backdrop-blur-xl fixed inset-0  w-screen h-screen flex items-center justify-center'>
+      <button className='fixed w-[30px] h-[30px] top-8 right-8'>
+        <div className='w-[25px] fixed border-2  rounded-lg border-black rotate-[45deg]'></div>
+        <div className='w-[25px] fixed border-2 rounded-lg border-black rotate-[-45deg]'></div>
+      </button>
       <div className='text-center w-3/4 h-1/4'>
         <EditForm id={id} sendEditedTask={sendEditedTask} />
       </div>
